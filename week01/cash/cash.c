68984511b339c538dac8a9cfe1f0488619a95f83
Tobias Pörtner <t.poertner@outlook.com>
2021-04-11 21:43:18 +0200

    cash

---
+++ week01/cash/cash.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int number_of_coins = 0;
+float get_float_above(int number, string prompt);
+int count_number_of_coins(int change_in_cents, int coin_size);
+
+int main(void)
+{
+	float change_in_dollars = get_float_above(0, "Change owed:");
+	int change_in_cents = round(change_in_dollars * 100);
+
+	change_in_cents = count_number_of_coins(change_in_cents, 25);
+	change_in_cents = count_number_of_coins(change_in_cents, 10);
+	change_in_cents = count_number_of_coins(change_in_cents, 5);
+	change_in_cents = count_number_of_coins(change_in_cents, 1);
+
+	printf("%i\n", number_of_coins);
+}
+
+float get_float_above(int number, string prompt)
+{
+    float n;
+    do
+    {
+        n = get_float("%s ", prompt);
+    }
+    while (n <= number);
+    return n;
+}
+
+int count_number_of_coins(int change_in_cents, int coin_size)
+{
+	while ( coin_size <= change_in_cents )
+	{
+		change_in_cents = change_in_cents - coin_size;
+		++number_of_coins;
+	}
+	return change_in_cents;
+}
