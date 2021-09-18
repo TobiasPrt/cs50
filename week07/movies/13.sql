e483ee15ea3d6d5e0fd1dbe670f23e13b559d18c
Tobias Pörtner <t.poertner@outlook.com>
2021-09-18 08:46:57 +0200

    movies done

---
+++ week07/movies/13.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT(name) FROM people 
+    JOIN stars ON people.id = stars.person_id 
+    JOIN movies ON stars.movie_id = movies.id 
+        WHERE 
+            NOT people.name = "Kevin Bacon"
+        AND
+            movies.id IN (SELECT movies.id FROM people 
+                JOIN stars ON people.id = stars.person_id 
+                JOIN movies ON stars.movie_id = movies.id 
+                    WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
